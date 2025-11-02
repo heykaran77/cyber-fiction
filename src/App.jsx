41ba94203f881e44430cebc15b89737c8cebb9c8
Karan Singh <devkaran.codes@gmail.com>
2025-11-02 20:27:52 +0530

    add section pinning

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PageSection from "./components/PageSection";
 import { useEffect, useRef } from "react";
 
 const App = () => {
+  // Lenis Scroll X GSAP Setup
   const lenisRef = useRef();
 
   useEffect(() => {
@@ -22,7 +23,13 @@ const App = () => {
       options={{ lerp: 0.1, smoothTouch: true, autoRaf: false }}
       ref={lenisRef}>
       <main className="relative overflow-hidden">
-        <PageSection id={1} bgColor={"bg-blue-500"} />
+        <PageSection
+          id={1}
+          bgColor="bg-blue-500"
+          className="flex flex-col items-center justify-center">
+          <h2 className="text-xl tracking-tighter">This is the child</h2>
+          <p className="text-sm text-blue-900 font-medium">Hope this renders</p>
+        </PageSection>
         <PageSection id={2} bgColor={"bg-red-500"} />
         <PageSection id={3} bgColor={"bg-yellow-500"} />
         <PageSection id={4} bgColor={"bg-purple-500"} />
--- src/components/PageSection.jsx
+++ src/components/PageSection.jsx
@@ -1,8 +1,32 @@
-const PageSection = ({ id, bgColor }) => {
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const PageSection = ({ id, bgColor, children, className = "" }) => {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      ScrollTrigger.create({
+        trigger: sectionRef.current,
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        scrub: 3,
+      });
+    }, sectionRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className={`h-screen flex items-center justify-center ${bgColor}`}>
-      Page {id}
-    </div>
+    <section
+      className={`h-screen flex items-center justify-center ${bgColor} ${className}`}
+      ref={sectionRef}>
+      {children || `Page ${id}`}
+    </section>
   );
 };
 
