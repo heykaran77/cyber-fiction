ef23d1063b74e0a81b409da10d0f7eab30f38c9a
Karan Singh <devkaran.codes@gmail.com>
2025-11-07 23:41:22 +0530

    add padding for mobile preloader

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -16,7 +16,7 @@ const PreLoader = ({ progress, isLoaded, onComplete }) => {
   }, [isLoaded, onComplete]);
 
   return (
-    <div className="preloader w-full h-screen fixed top-0 left-0 bg-[#0c0c0c] z-50 flex justify-end items-end px-8 py-8">
+    <div className="preloader w-full h-screen fixed top-0 left-0 bg-[#0c0c0c] z-50 flex justify-end items-end px-8 md:py-8 py-16">
       <h1 className="text-orange-400 text-8xl tracking-tight font-semibold">
         {String(progress).padStart(2, "0")}
       </h1>
