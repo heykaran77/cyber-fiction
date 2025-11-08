0ba4c53a0ded9c56da1a06e7cca900ec87ba43ff
Karan Singh <devkaran.codes@gmail.com>
2025-11-08 13:19:45 +0530

    add config for oneko

--- src/components/OnekoCat.jsx
+++ src/components/OnekoCat.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react";
+import { catConfig } from "../configs/CatCon.js";
 
 export default function OnekoCat() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "./oneko/oneko.js";
-    script.dataset.cat = "./oneko/oneko.gif";
-    script.async = true;
-    document.body.appendChild(script);
+    if (catConfig.enabled) {
+      const script = document.createElement("script");
+      script.src = "./oneko/oneko.js";
+      script.dataset.cat = "./oneko/oneko.gif";
+      script.async = true;
+      document.body.appendChild(script);
 
-    return () => {
-      // Clean up if component unmounts
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
-    };
+      return () => {
+        // Clean up if component unmounts
+        if (script.parentNode) {
+          script.parentNode.removeChild(script);
+        }
+      };
+    }
+    return () => null;
   }, []);
 
   return null; // no visible element needed
